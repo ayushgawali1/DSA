ab9cad6d9e5e617cb291a44283d4c5929b5b0e40
Ayush Gawali <ayushgawali.12345@gmail.com>
2025-02-26 12:05:33 +0530

    Day1 Folder added

---
+++ Day1/FirstProg.java
@@ -0,0 +1,8 @@
+package Day1;
+
+
+class FirstProg {
+    public static void main(String args[]) {
+        System.out.println("Hello World");
+    }
+}
---
+++ Day1/Readme.md
@@ -0,0 +1,2 @@
+<h1>Day 1</h1>
+<p>It conatin first Java program </p>
